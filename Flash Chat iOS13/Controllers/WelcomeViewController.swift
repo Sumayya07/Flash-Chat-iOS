e914ea40bdac795c3649fe6e8310972f931c2c28
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-03-01 20:58:30 +0530

    Creadted Typing Animations and Timer

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,6 +14,18 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = ""
+        var charIndex = 0.0 // This variable is helping us to change the timer interval so that it take accounts a delay for every subsequest letter.
+        let titleText = "⚡️FlashChat"
+        for letter in titleText { // This is a for loop
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (Timer) in
+                self.titleLabel.text?.append(letter) // For every letter in the title text Timer property schedules a timer that waits for 0.1 second and then it adds the letter to our titlelabel.text.
+            }
+            charIndex += 1
+            // THE ABOVE CODE IS HELPING US TO CREATE A TYPING ANIMATION WHEN THE USER OPENS THE APP.
+            
+        }
 
        
     }
